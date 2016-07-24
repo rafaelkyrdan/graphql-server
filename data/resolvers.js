de66c8f6fac3245f15757ee2cea44be47abaae4a
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-07-24 19:09:57 +0300

    Added connector to sqlite

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,20 +1,19 @@
+import { Author } from './connectors';
+
 const resolvers = {
   Query: {
-    author(root, args){
-      return { id: 1, firstName: 'Hello', lastName: 'World' };
+    author(_, args) {
+      return Author.find({ where: args });
     },
   },
   Author: {
-    posts(author){
-      return [
-        { id: 1, title: 'A post', text: 'Some text', views: 2},
-        { id: 2, title: 'Another post', text: 'Some other text', views: 200}
-      ];
+    posts(author) {
+      return author.getPosts();
     },
   },
   Post: {
-    author(post){
-      return { id: 1, firstName: 'Hello', lastName: 'World' };
+    author(post) {
+      return post.getAuthor();
     },
   },
 };
