9bd4dc1124e48d119ba2f5a43cce67b6a05f5073
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-07-24 19:47:55 +0300

    added connector to mongodb

--- data/connectors.js
+++ data/connectors.js
@@ -1,6 +1,9 @@
 import Sequelize from 'sequelize';
 import casual from 'casual';
 import _ from 'lodash';
+import Mongoose from 'mongoose';
+
+// SQLit connector
 
 const db = new Sequelize('blog', null, null, {
   dialect: 'sqlite',
@@ -21,6 +24,7 @@ AuthorModel.hasMany(PostModel);
 PostModel.belongsTo(AuthorModel);
 
 // create mock data with a seed, so we always get the same
+/*
 casual.seed(123);
 db.sync({ force: true }).then(() => {
   _.times(10, () => {
@@ -35,8 +39,43 @@ db.sync({ force: true }).then(() => {
     });
   });
 });
+*/
 
 const Author = db.models.author;
 const Post = db.models.post;
 
-export { Author, Post };
+
+// MongoDB connector
+
+const mongo = Mongoose.connect('mongodb://localhost/views');
+
+const ViewSchema = Mongoose.Schema({
+  postId: Number,
+  views: Number,
+});
+
+const View = Mongoose.model('views', ViewSchema);
+
+// modify the mock data creation to also create some views:
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return AuthorModel.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).then((author) => {
+      return author.createPost({
+        title: `A post by ${author.firstName}`,
+        text: casual.sentences(3),
+      }).then((post) => { // <- the new part starts here
+        // create some View mocks
+        return View.update(
+          { postId: post.id },
+          { views: casual.integer(0, 100) },
+          { upsert: true });
+      });
+    });
+  });
+});
+
+export { Author, Post, View };
